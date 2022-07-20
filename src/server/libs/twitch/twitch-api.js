20acd3708d82eb8be6984573833542a90e37c993
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-07-19 21:36:42 -0700

    added @jlengstorf/get-twitch-oauth to getOAuth Token

--- src/server/libs/twitch/twitch-api.js
+++ src/server/libs/twitch/twitch-api.js
@@ -1,16 +1,23 @@
 const { get } = require("axios");
 require("dotenv").config();
+const { getTwitchAccessToken } = require("@jlengstorf/get-twitch-oauth");
 
 const TWITCH_ENDPOINT = "https://api.twitch.tv/helix";
 
 const getUserId = async userName => {
   const URL = TWITCH_ENDPOINT + "/users?login=" + userName;
 
+  const { access_token: accessToken } = await getTwitchAccessToken({
+    client_id: process.env.TWITCH_CLIENT_ID,
+    client_secret: process.env.TWITCH_CLIENT_SECRET,
+    scopes: "user:read:email"
+  });
+
   try {
     const response = await get(URL, {
       headers: {
         "Client-ID": process.env.TWITCH_CLIENT_ID,
-        Authorization: `Bearer ${process.env.TWITCH_OAUTH_TOKEN}`
+        Authorization: `Bearer ${accessToken}`
       }
     });
 
@@ -25,11 +32,17 @@ const getUserId = async userName => {
 const getUserName = async userId => {
   const URL = TWITCH_ENDPOINT + "/users?id=" + userId;
 
+  const { access_token: accessToken } = await getTwitchAccessToken({
+    client_id: process.env.TWITCH_CLIENT_ID,
+    client_secret: process.env.TWITCH_CLIENT_SECRET,
+    scopes: "user:read:email"
+  });
+
   try {
     const response = await get(URL, {
       headers: {
         "Client-ID": process.env.TWITCH_CLIENT_ID,
-        Authorization: `Bearer ${process.env.TWITCH_OAUTH_TOKEN}`
+        Authorization: `Bearer ${accessToken}`
       }
     });
 
