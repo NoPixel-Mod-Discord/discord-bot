e549bf1d64920f2b08d4f1bc7b838ffbddf0dd20
iRoyalDev <iroyaldev@gmail.com>
2022-08-16 16:56:45 -0400

    Adding feature BOLO
    Single command giving option for either URL input, or an attachment.

---
+++ bot/commands/bolo.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, MessageEmbed, MessageAttachment, AttachmentBuilder } = require('discord.js');
+
+module.exports = {
+        data: new SlashCommandBuilder()
+                .setName('bolo')
+                .setDescription('BE ON THE LOOKOUT! (URL or attachment required)')
+                  .addStringOption((option) => option.setName('user').setDescription('Username of offender.').setRequired(true))
+                  .addStringOption((option) => option.setName('reason').setDescription('Reason for the BOLO.').setRequired(true))
+                  .addStringOption((option) => option.setName('url').setDescription('URL to the screenshot of the offensive content. (Limited to 1)').setRequired(false))
+                  .addAttachmentOption((option) => option.setName('attachment').setDescription('Screenshot of the offensive content. (Limited to 1)').setRequired(false)),
+
+        async execute(interaction, client) {
+
+        const attachment = interaction.options.getAttachment('attachment');
+        const user = interaction.options.getString('user');
+        const reason = interaction.options.getString('reason');
+        const image = (interaction.options.getString('url') !== null) ? interaction.options.getString('url') : attachment.url;
+
+        if (user && reason && image) {
+        const embed = new MessageEmbed()
+        .setColor('RED')
+        .setTitle('** BE ON THE LOOKOUT **')
+        .addField('Username:', user)
+        .addField('Reason:', reason)
+        .setImage(`${image}`)
+
+        let msgEmbed = await interaction.reply({embeds: [embed]});
+
+        } else {
+            await interaction.reply("Error, please notify a bot dev.");
+          }
+        },
+};
