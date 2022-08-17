6adfb24c3f7121613362ed0fa21d05fba687e857
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-08-17 03:06:39 -0700

    removed unused code and error handling(bolo command)

--- bot/commands/bolo.js
+++ bot/commands/bolo.js
@@ -1,15 +1,15 @@
-const { Client, MessageEmbed, MessageAttachment, AttachmentBuilder, SlashCommandBuilder } = require('discord.js');
+const { MessageEmbed, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
         data: new SlashCommandBuilder()
-                .setName('bolo')
-                .setDescription('BE ON THE LOOKOUT! (URL or attachment required)')
+                .setName('addbolo')
+                .setDescription('BE ON THE LOOKOUT! (URL or attachment either can be used but required)')
                   .addStringOption((option) => option.setName('user').setDescription('Username of offender.').setRequired(true))
                   .addStringOption((option) => option.setName('reason').setDescription('Reason for the BOLO.').setRequired(true))
                   .addStringOption((option) => option.setName('url').setDescription('URL to the screenshot of the offensive content. (Limited to 1)').setRequired(false))
                   .addAttachmentOption((option) => option.setName('attachment').setDescription('Screenshot of the offensive content. (Limited to 1)').setRequired(false)),
 
-        async execute(interaction, client) {
+        async execute(interaction) {
 
         const attachment = interaction.options.getAttachment('attachment');
         const user = interaction.options.getString('user');
@@ -23,7 +23,11 @@ module.exports = {
         .addField('Reason:', reason)
         .setImage(`${image}`)
 
-        let msgEmbed = await interaction.reply({embeds: [embed]});
+         await interaction.reply({embeds: [embed]});
+                
+         if (user || reason || image  === null) {
+             await interaction.reply("Please input all fields.")
+         }
 
         },
 };
